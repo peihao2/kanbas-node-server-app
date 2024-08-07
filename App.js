6844a586f02d63a6a4215ad9278d23a3ac4b12fe
peihao2 <l709811890@gmail.com>
2024-08-07 15:17:17 -0700

    a5

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import express from "express";
-import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
@@ -11,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
-Hello(app);
+
 Lab5(app);
 AssignmentRoutes(app);
 app.listen(4000);
