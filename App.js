aa5287cd55e714bf48dc022b401b44ca0fb3246b
peihao2 <l709811890@gmail.com>
2024-08-06 17:29:22 -0700

    a5

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-// const express = require('express')
 import express from "express";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
+import cors from "cors";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+
 const app = express();
-
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
-});
-
+app.use(cors());
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
+Hello(app);
+Lab5(app);
+AssignmentRoutes(app);
 app.listen(4000);
