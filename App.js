471ecb0c315906c8846dba37b6ccd970c2f9870b
peihao2 <l709811890@gmail.com>
2024-08-07 13:36:50 -0700

    a5

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Hello from "Hello.js";
+import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
